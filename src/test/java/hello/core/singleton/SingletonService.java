d950f70e331bc202421f1d39d33c314ba9db08a4
JisooHan <jisoohan@soleo.kr>
2023-10-05 18:35:34 +0900

    섹션 5. 싱글톤 컨데이너
    [Note]
    객체는 stateless 하게 유지
    Spring boot 가 자동적으로 Singletone 유지 시켜줌

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,18 @@
+package hello.core.singleton;
+
+public class SingletonService {
+
+    // 1. 1개만 생성
+    private static final SingletonService instance = new SingletonService();
+
+    public static SingletonService getInstance(){
+        return instance;
+    }
+
+    private SingletonService(){
+
+    }
+    public  void logic(){
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}

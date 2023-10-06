529f1dc4805260ef8202bf1689cace1f96aba910
JisooHan <hanjisoo93@gmail.com>
2023-10-06 14:37:24 +0900

    섹션 6. 컴포넌트 스캔: 완료

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,13 +1,26 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // 1. 직접 지정 가능
+        //basePackages = "hello.core.member",
+        //basePackageClasses = AutoAppConfig.class,
+
+        //2. 직접 지정 하지 않으면, Default 는 AutoAppConfig 의 하위 package 부터
+
         // 수동 등록 건들은 제외: 예제 코드를 살리기 위해...
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }

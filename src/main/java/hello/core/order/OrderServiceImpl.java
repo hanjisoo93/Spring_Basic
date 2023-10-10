8b4d88e89ffa938098cc1c96d367be777e5b31e3
JisooHan <hanjisoo93@gmail.com>
2023-10-10 17:11:36 +0900

    섹션 7. 의존관계 자동 주입
    [Note]
    1. 편리한 자동 기능은 기본으로 사용하기
    2. 직접 등록하는 기술 지원 개체는 수동 등록
    3. 다형성을 적극 활용하는 비지니스 로직은 수동 등로 고려 --> 특정패키지에 같이 묶어 두는것이 좋다

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package hello.core.order;
 
 import hello.core.Discount.DiscountPolicy;
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,7 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
@@ -26,8 +27,8 @@ public class OrderServiceImpl implements OrderService{
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
-    //테스트 용도
-    public MemberRepository getMemberRepository(){
-        return memberRepository;
-    }
+//    //테스트 용도
+//    public MemberRepository getMemberRepository(){
+//        return memberRepository;
+//    }
 }
